7910055c99bb04d69090fc217a262eb5cfbddcda
Anirudh Sriram <anirudh.sriram96@gmail.com>
2022-12-10 13:47:19 -0500

    Create supervisor with named registry No need for supervisor name when calling registry client methods

---
+++ lib/kv/supervisor.ex
@@ -0,0 +1,20 @@
+defmodule KV.Supervisor do
+  # Supervisor can start processes, deal with failure
+  # and shut down processes
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  @impl true
+  def init(:ok) do
+    children = [
+      {KV.Registry, name: KV.Registry}
+    ]
+
+    # Invokes child_spec/1 on each child (automatically created on `use Agent`, `use Registry` or `use Supervisor`)
+    # name passed to opts of KV.Registry.start_link
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
